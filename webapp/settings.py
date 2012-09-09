1078e012a788561f7d23ff02bdec53c91b9fa16d
James Pennell <pennell.james@gmail.com>
2012-09-08 22:46:08 -0400

    Add admin

--- webapp/settings.py
+++ webapp/settings.py
@@ -111,10 +111,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gunicorn',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -145,3 +143,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
